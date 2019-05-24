c58298d01e9d2fcbd2e8850d302c7da4f8a5ec6f
Alain <alain@fxstreet.com>
2019-05-25 01:27:05 +0200

    swagger

--- src/ControlHorario.Api/Startup.cs
+++ src/ControlHorario.Api/Startup.cs
@@ -11,11 +11,15 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ControlHorario.Api
 {
     public class Startup
     {
+        const string SwaggerName = "ControlHorario.Api";
+        const string SwaggerVersion = "v1";
+        
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -36,6 +40,13 @@ namespace ControlHorario.Api
 
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(SwaggerVersion, new Info {
+                    Title = SwaggerName, Version = SwaggerVersion
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +61,14 @@ namespace ControlHorario.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(
+                    $"/swagger/{SwaggerVersion}/swagger.json",
+                    $"{SwaggerName} {SwaggerVersion}");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
