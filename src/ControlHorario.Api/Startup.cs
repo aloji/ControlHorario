78eba155e8af607ce041bfe78e3b2b43a6365d9d
Alain <alain@fxstreet.com>
2019-05-30 18:10:11 +0200

    UseCors

--- src/ControlHorario.Api/Startup.cs
+++ src/ControlHorario.Api/Startup.cs
@@ -52,6 +52,8 @@ namespace ControlHorario.Api
                     Title = SwaggerName, Version = SwaggerVersion
                 });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +68,12 @@ namespace ControlHorario.Api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseCors(
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
