eb15e0c0ca2a4cb0555286ac747182e6deea3204
Alain <alain@fxstreet.com>
2019-08-23 16:15:23 +0200

    alert in signup

--- src/ControlHorario.Web/js/signup.js
+++ src/ControlHorario.Web/js/signup.js
@@ -5,6 +5,18 @@ Vue.component("person-data", {
     }
 });
 
+Vue.component("alert", {
+    template: '#alert',
+    props: {
+        error: null
+    },
+    computed: {
+        classObject: function () {
+            return  "alert-" + this.error.level;
+        }
+    }
+});
+
 Vue.component("capture", {
     template: '#capture',
     props: ['url'],
@@ -16,12 +28,15 @@ Vue.component("capture", {
     },
     mounted: function () {
         this.video = this.$refs.video;
+        const self = this;
         navigator.mediaDevices.getUserMedia({video: true, audio: false})
             .then(function(stream) {
                 this.video.srcObject = stream;
                 this.video.play();
+                self.$emit("removealert");
             })
             .catch(function(err) {
+                self.$emit("showerror", err);
                 console.log("An error occurred: " + err);
             });
     },
@@ -31,17 +46,21 @@ Vue.component("capture", {
             const context = canvas.getContext("2d");
             context.drawImage(this.video, 0, 0, canvas.width, canvas.height);
             this.taked = true;
+            this.$emit("removealert");
         },
         upload(){
             const canvas = this.$refs.canvas;
             const img = canvas.toDataURL("image/png");
-
+            const self = this;
             const config = { headers: {'Content-Type': 'application/json'} };
             axios.post(this.url, "\"" + img + "\"", config)
                 .then(function (response) {
+                    self.taked = false;
+                    self.$emit("showinfo", "the capture has been uploaded");
                     console.log(response);
                 })
                 .catch(function (error) {
+                    self.$emit("showerror", error);
                     console.log(error);
                 });
         }
@@ -64,7 +83,8 @@ var app = new Vue({
         }
     },
     data: {
-        person: null
+        person: null,
+        log: null
     },
     created: function () {
         const id = this.$route.params.personid
@@ -77,10 +97,27 @@ var app = new Vue({
             axios.get(uri)
                 .then(function (response) {
                     self.person = response.data;
+                    self.onRemoveAlert();
               })
               .catch(function (error) {
                     console.log(error);
+                    self.onShowError("user not found");
               });
+        },
+        onShowError(msg){
+            this.log = {
+                level: "danger",
+                msg: msg
+            };
+        },
+        onShowInfo(msg){
+            this.log = {
+                level: "info",
+                msg: msg
+            };
+        },
+        onRemoveAlert(){
+            this.log = null;
         },
         getAddFaceUrl(){
             if(this.person){
